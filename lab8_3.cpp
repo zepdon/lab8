d26efb817c5d594a05c1750b030050759ec6a401
Poon <zeepengineer@gmail.com>
2023-12-15 13:01:43 +0700

    lab8_3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,56 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int age , height , bounty;
+string character;
+
+int main(){
+    cout << "Enter your age: ";
+    cin >> age;
+    //cout << endl;
+    if (age <=25)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        //cout << endl;
+        if (height < 100)
+        {
+            character = "Chopper";
+        } else if (height < 180 ){
+            character = "Usopp";
+        } else {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            //cout << endl;
+            if (bounty <= 1100000000)
+            {
+                character = "Sanji";
+            } else {
+                character = "Zoro";
+            }
+            
+        }
+        
+    } else if (age <= 60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        //cout << endl;
+        if (bounty <= 500000000)
+        {
+            character = "Franky";
+        } else {
+            character = "Jinbe";
+        }
+        
+    } else {
+        character = "Brook";
+    }
+    
+    cout << "Your character = "<< character;
+}
